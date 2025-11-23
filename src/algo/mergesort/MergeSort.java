50ba6943a81979dba9eae06eeeb814a2d7928436
ggan-au <glenganderton@proton.me>
2025-11-23 17:19:23 +1100

    ALGO - merge sort - re-write for additional practice - 2

--- src/algo/mergesort/MergeSort.java
+++ src/algo/mergesort/MergeSort.java
@@ -4,42 +4,40 @@ public class MergeSort {
 
     public static void mergeSort(int[] inputArray) {
         int inputSize = inputArray.length;
-        if (inputSize < 2) {
-            return;
-        }
+        if (inputSize < 2) return;
 
         int midPoint = inputSize / 2;
-        int[] leftSide = new int[midPoint];
-        int[] rightSide = new int[inputSize - midPoint];
+        int[] leftArray = new int[midPoint];
+        int[] rightArray = new int[inputSize - midPoint];
 
-        for (int i = 0; i < leftSide.length; i++) {
-            leftSide[i] = inputArray[i];
+        for (int i = 0; i < midPoint; i++) {
+            leftArray[i] = inputArray[i];
         }
 
-        for (int i = 0; i < rightSide.length; i++) {
-            rightSide[i] = inputArray[midPoint + i];
+        for (int i = 0; i < rightArray.length; i++) {
+            rightArray[i] = inputArray[midPoint + i];
         }
 
-        mergeSort(leftSide);
-        mergeSort(rightSide);
-        merge(inputArray, leftSide, rightSide);
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+        merge(inputArray, leftArray, rightArray);
     }
 
-    public static void merge(int[] inputArray, int[] leftSide, int[] rightSide) {
-        int leftSize = leftSide.length;
-        int rightSize = rightSide.length;
+    public static void merge(int[] inputArray, int[] leftArray, int[] rightArray) {
         int i = 0, j = 0, k = 0;
+        int leftSize = leftArray.length;
+        int rightSize = rightArray.length;
 
         while (i < leftSize && j < rightSize) {
-            if (leftSide[i] < rightSide[j]) {
-                inputArray[k++] = leftSide[i++];
+            if (leftArray[i] < rightArray[j]) {
+                inputArray[k++] = leftArray[i++];
             } else {
-                inputArray[k++] = rightSide[j++];
+                inputArray[k++] = rightArray[j++];
             }
         }
 
-        while (i < leftSize) inputArray[k++] = leftSide[i++];
-        while (j < rightSize) inputArray[k++] = rightSide[j++];
+        while (i < leftSize) inputArray[k++] = leftArray[i++];
+        while (j < rightSize) inputArray[k++] = rightArray[j++];
     }
 
 }
